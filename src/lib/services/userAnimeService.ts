e5b155c3d00c8f31860f227ded9dd2a30fbe5414
nutzamu <nut59607@gmail.com>
2026-05-08 23:38:11 +0700

    add fetchUserAnimeListByUserId

--- src/lib/services/userAnimeService.ts
+++ src/lib/services/userAnimeService.ts
@@ -41,8 +41,40 @@ export async function fetchUserAnimeListByUsername(username: string) {
   }));
 }
 
-// map ให้ตรง client
+export async function fetchUserAnimeListByUserId(userId: string) {
 
+  const list = await prisma.userAnime.findMany({
+    where: { userId: userId },
+    include: { anime: true },
+    orderBy: { updatedAt: 'desc' },
+  });
+
+  return list.map((entry) => ({
+    id: entry.id,
+    status: toClientStatus(entry.status) as any,
+    score: entry.score,
+    progress: entry.progress,
+    dateAdded: entry.createdAt.toISOString(),
+
+    anime: {
+      id: entry.anime.id,
+      anilistId: entry.anime.anilistId,
+      title: entry.anime.title,
+      genres: entry.anime.genres,
+      coverImage: entry.anime.coverImage,
+      episodes: entry.anime.episodes ?? undefined,
+      year: entry.anime.year ?? undefined,
+      season: entry.anime.season ?? undefined,
+      status: toFEStatus(entry.status),
+      averageScore: entry.anime.averageScore ?? undefined,
+      studio: entry.anime.studio ?? undefined,
+      description: entry.anime.description ?? undefined,
+      trailer: entry.anime.trailer ?? undefined,
+    },
+  }));
+}
+
+// map ให้ตรง client
 function toClientStatus(status: string) {
   switch (status) {
     case 'PLANNING':
